c1e69b7106653116b27b572e6965a28f2cef5d34
Rolin Blake <rolinmblake@gmail.com>
2024-09-04 11:13:47 -0500

    generate_tseries_plot first implementation

--- src/graphing.rs
+++ src/graphing.rs
@@ -2,14 +2,41 @@ use anyhow::{Context, Result};
 use super::avantage::tseries_from_csv;
 use super::options::chain_from_csv;
 use std::process::{Command, Stdio};
-use std::fs::File;
 use std::io::{Write, BufWriter};
+use std::fs::File;
 
 const CDATNAME: &str = "dat_out/ctemp.dat";
 const PDATNAME: &str = "dat_out/ptemp.dat";
 
-pub fn generate_tseries_plot(ts_csv_name: &str) -> Result<()> {
-    let tseries = tseries_from_csv(ts_csv_name);
+pub fn generate_tseries_plot(ts_csv_name: &str, png_name: &str) -> Result<()> {
+    let data_label: &str = "Close Price";
+    let name_parts: Vec<&str> = ts_csv_name.split('/').collect();
+    let ticker = name_parts[1].split('_').collect::<Vec<&str>>()[0];
+    let gnuplot_script = format!(
+        r#"
+        set terminal png
+        set output '{}'
+        set datafile separator ','
+        set xdata time
+        set timefmt '%Y-%m-%d'
+        set format x "%m/%d"
+        set xlabel "Date"
+        set ylabel "{}"
+        set title "{} {}"
+        set grid
+        set key autotitle columnheader
+        plot '{}' using "Date":"Close" with lines title '{}'
+        "#, png_name, data_label, ticker, data_label, ts_csv_name, data_label);
+    let mut cmd_gnuplot = Command::new("gnuplot")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .context("\ngenerate_tseries_plot() :: ERROR -> Failed to execute cmd_gnuplot chart generation command")?;
+    let stdin = cmd_gnuplot.stdin.as_mut().context("\ngenerate_tseries_plot() :: ERROR -> Failed to open stdin for gnuplot_script")?;
+    writeln!(stdin, "{}", gnuplot_script).context("\ngenerate_tseries_plot() :: ERROR -> Failed to write gnuplot_script to stdin for chart")?;
+    cmd_gnuplot.wait().context("\ngenerate_tseries_plot() :: ERROR -> Failed to wait for gnuplot chart generation process")?;
+    println!("\ngenerate_surface_plot() :: Successfully generated {}", png_name);
     Ok(())
 }
 
@@ -54,7 +81,7 @@ pub fn generate_surface_plot(chain_csv_name: &str, call_png_name: &str, put_png_
         .spawn()
         .context("\ngenerate_surface_plot() :: ERROR -> Failed to execute cmd_call gnuplot surface generation command")?;
     let stdin = cmd_call.stdin.as_mut().context("\ngenerate_surface_plot() :: ERROR -> Failed to open stdin for gnuplot_cscript")?;
-    writeln!(stdin, "{}", gnuplot_cscript).context("\ngenerate_surface_plot() :: ERROR -> Failed to write gnuplot_cscript for call surface")?;
+    writeln!(stdin, "{}", gnuplot_cscript).context("\ngenerate_surface_plot() :: ERROR -> Failed to write gnuplot_cscript to stdin for call surface")?;
     cmd_call.wait().context("\ngenerate_surface_plot() :: ERROR -> Failed to wait for gnuplot call surface generation process")?;    
     println!("\ngenerate_surface_plot() :: Successfully generated {}", call_png_name);
     let gnuplot_pscript = format!(
@@ -77,7 +104,7 @@ pub fn generate_surface_plot(chain_csv_name: &str, call_png_name: &str, put_png_
         .spawn()
         .context("\ngenerate_surface_plot() :: ERROR -> Failed to execute cmd_put gnuplot surface generation command")?;
     let stdin = cmd_put.stdin.as_mut().context("\ngenerate_surface_plot() :: ERROR -> Failed to open stdin for gnuplot_pscript")?;
-    writeln!(stdin, "{}", gnuplot_pscript).context("\ngenerate_surface_plot() :: ERROR -> Failed to write gnuplot_pscript for call surface")?;
+    writeln!(stdin, "{}", gnuplot_pscript).context("\ngenerate_surface_plot() :: ERROR -> Failed to write gnuplot_pscript to stdin for put surface")?;
     cmd_put.wait().context("\ngenerate_surface_plot() :: ERROR -> Failed to wait for gnuplot put surface generation process")?;    
     println!("\ngenerate_surface_plot() :: Successfully generated {}", put_png_name);
     Ok(())
