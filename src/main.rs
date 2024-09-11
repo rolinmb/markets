6490ad70e42c0b22910b6f5a165ffeda68d8a381
Rolin Blake <rolinmblake@gmail.com>
2024-09-11 10:50:06 -0500

    not using option chain stuff right now

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use finviz::{fetch_finviz_info};
 mod avantage;
 use avantage::{get_underlying_av};
 mod finmath;
-mod options;
-use options::{fetch_option_chain};
+/*mod options;
+use options::{fetch_option_chain};*/
 mod graphing;
-use graphing::{generate_tseries_plot, generate_surface_plot, plot_volatility_smiles};
+use graphing::{generate_tseries_plot/*, generate_surface_plot, plot_volatility_smiles*/};
 mod utils;
 use utils::{clear_directory_or_create, create_directory_if_dne};
 use std::process::{Command, exit};
@@ -34,17 +34,17 @@ fn main() {
         let datetime_str = now.format("%Y-%m-%d_%H-%M-%S").to_string();
         let fv_csv = format!("{}{}_fv_{}.csv", CSVDIR, uticker, datetime_str);
         let av_csv = format!("{}{}_av_{}.csv", CSVDIR, uticker, datetime_str);
-        let oc_csv = format!("{}{}_oc_{}.csv", CSVDIR, uticker, datetime_str);
+        //let oc_csv = format!("{}{}_oc_{}.csv", CSVDIR, uticker, datetime_str);
         let _ = fetch_finviz_info(&uticker, &fv_csv);
         let _ = get_underlying_av(&uticker, &av_csv);
-        let _ = fetch_option_chain(&uticker, &oc_csv);
+        //let _ = fetch_option_chain(&uticker, &oc_csv);
         for series_field in 0..11 {
             let _ = generate_tseries_plot(&av_csv, series_field);
         }
-        for plot_field in 0..24 {
+        /*for plot_field in 0..24 {
             let _ = generate_surface_plot(&oc_csv, plot_field);
         }
-        let _ = plot_volatility_smiles(&oc_csv);
+        let _ = plot_volatility_smiles(&oc_csv);*/
         let pdf_cmd = Command::new("cmd")
             .args(["/C", "python", "scripts/main.py", &uticker, &datetime_str])
             .output()
